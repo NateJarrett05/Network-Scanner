5b0dbea28c7ef4e76914dc38e25e3415f2184c62
Nate <natejarrett05@gmail.com>
2024-10-14 18:04:21 -0400

    Device scanner implemented. Non-functioning

--- main.py
+++ main.py
@@ -5,6 +5,33 @@ import arguments
 
 # arguments.get_args()
 
+# Retrieves IP and MAC address pairs for devices on a target IP
+def device_scanner(target):
+    # IP address for the destination
+    target_IP = target
+    # target_IP = gethostbyname(target)
+    print('Starting scan on host: ', target_IP)
+    # Create ARP packet
+    arp = scapy.ARP(pdst = target_IP)
+    # Create the Ether broadcast packet
+    # ff:ff:ff:ff:ff:ff MAC Address indicates broadcasting
+    ether = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    # Stack the packets
+    packet = ether/arp
+
+    result = scapy.srp(packet, timeout=5)[0]
+
+    # List of clients
+    clients = []
+    for sent, received in result:
+        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+    print("Available devices in the network:")
+    print("IP" + " "*18+"MAC")
+    for client in clients:
+        print("{:16}    {}".format(client['ip'], client['mac']))
+
+# Scans for open ports on a target IP
 # Works on localhost, not sure about external hosts
 def port_scanner(target):
     startTime = time.time()
@@ -24,4 +51,4 @@ def port_scanner(target):
         s.close()
     print('Time taken:', time.time() - startTime)
 
-port_scanner('localhost')
+device_scanner('192.168.1.1/24')
